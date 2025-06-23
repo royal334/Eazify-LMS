9983eed52ae6d218ea1779d8954ff01841c841c7
Jude-Ugoo <ugooeze0@gmail.com>
2025-06-23 23:10:57 +0100

    project setup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  getHealth(): string {
+    return this.appService.getHealth();
+  }
+
+  @Get('config')
+  getConfig(): any {
+    return {
+      port: this.configService.get('PORT'),
+      dbUrl: this.configService.get('DATABASE_URL'),
+      supabaseUrl: this.configService.get('SUPABASE_URL'),
+    };
+  }
+
+  @Get('prisma-test')
+  async getPrismaTest(): Promise<{ status: string }> {
+    await this.prismaService.$queryRaw`SELECT 1`;
+    return { status: 'Prisma Connected' };
   }
 }
